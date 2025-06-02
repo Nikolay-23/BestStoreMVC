90967102591339d44768e71deaa0b5a4a81232c6
Nikolay-23 <115502655+Nikolay-23@users.noreply.github.com>
2025-06-02 00:42:50 +0000

    Edit option

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,95 @@
+﻿using BestStoreMVC.Models;
+using BestStoreMVC.Services;
+using BestStoreMVC.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BestStoreMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _environment;
+        public ProductsController(ApplicationDbContext context, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.OrderByDescending(i => i.Id).ToList(); //Retrive all movies from the database
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new ProductViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductViewModel viewModel)
+        {
+            //Validate the input data using ModelState
+            if (ModelState.IsValid)
+            {
+                // save the image file
+                string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                newFileName += Path.GetExtension(viewModel.ImageFile!.FileName);
+
+                string imageFullPath = _environment.WebRootPath + "/images/" + newFileName;
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    viewModel.ImageFile.CopyTo(stream);
+                }
+
+                //Map the view model to the Product entity
+                var product = new Product
+                {
+                    Name = viewModel.Name,
+                    Brand = viewModel.Brand,
+                    Category = viewModel.Category,
+                    Price = viewModel.Price,
+                    Description = viewModel.Description,
+                    ImageFileName = newFileName,
+                    CreatedAt = DateTime.Now
+                };
+
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Products");
+            }
+
+            
+
+            return View(viewModel);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            // create productDto from product
+            var productDto = new ProductViewModel
+            {
+                Name = product.Name,
+                Brand = product.Brand,
+                Category = product.Category,
+                Price = product.Price,
+                Description = product.Description
+            };
+
+            ViewData["ProductId"] = product.Id;
+            ViewData["ImageFileName"] = product.ImageFileName;
+            ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yyyy");
+
+            return View(productDto);
+        }
+    }
+}
---
+++ ViewModel/ProductViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static BestStoreMVC.Common.EntityValidationConstants;
+
+namespace BestStoreMVC.ViewModel
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required!")]
+        [MaxLength(NameMaxLength, ErrorMessage = "Product Name is too long!")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Product Brand is required!")]
+        [MaxLength(BrandMaxLength, ErrorMessage = "Product Brand is too long!")]
+        public string Brand { get; set; } = null!;
+
+        [Required(ErrorMessage = "Category Name is required!")]
+        [MaxLength(CategoryMaxLength, ErrorMessage = "Product Category is too long!")]
+        public string Category { get; set; } = null!;
+
+        [Required(ErrorMessage = "Product Price is required!")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Product Description is required!")]
+        public string Description { get; set; } = null!;
+
+        [Required(ErrorMessage = "The image file is required!")]
+        public IFormFile? ImageFile { get; set; } = null!;
+    }
+}
